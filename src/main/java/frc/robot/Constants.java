83ae78cff14670b8a2ab8cc2df5a50c73cf8dbca
xeldridge18 <xeldridge13@gmail.com>
2026-03-14 08:37:30 -0500

    Update at 'Sat Mar 14 08:37:29 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -158,14 +158,14 @@ public static final double LOOKAHEAD_MAX_SEC   = 1.5;
     public static final double IDLE = 0; // % voltage -1 --> 1
 
     // PID Constants For Shooter
-    public static final double p = 0.0008;
+    public static final double p = 0.0002355;
     public static final double i = 0.000;
-    public static final double d = 3;
+    public static final double d = 0;
 
     // Feed-Forward Constants for Shooter
     public static final double s = 0.100;
-    public static final double v = 0.03;
-    public static final double a = 0.000;
+    public static final double v = 0.00177;
+    public static final double a = 0.00017;
 
     public final static InterpolatingDoubleTreeMap TOF = new InterpolatingDoubleTreeMap();
 
