bef03a07357e24f471e982b5f0182e0a37e87aff
xeldridge18 <xeldridge13@gmail.com>
2026-03-30 16:00:02 -0500

    Update at 'Mon Mar 30 16:00:02 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -127,8 +127,8 @@ public static final double LOOKAHEAD_MAX_SEC   = 1.5;
 
     public static final double OUTTAKE_SPEED = -1;
     public static final double INTAKE_SPEED = 1;
-     public static final double OUTTAKE_RPM = -6000;
-    public static final double INTAKE_RPM = 6000;
+     public static final double OUTTAKE_RPM = -15000;
+    public static final double INTAKE_RPM = 15000;
 
   }
 
