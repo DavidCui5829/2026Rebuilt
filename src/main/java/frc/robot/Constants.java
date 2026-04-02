0f82079de6ed87e6eb0e1f4077fde650bf7886be
xeldridge18 <xeldridge13@gmail.com>
2026-04-01 19:29:41 -0500

    Update at 'Wed Apr 01 19:29:40 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -168,12 +168,12 @@ public static final double LOOKAHEAD_MAX_SEC   = 1.5;
     public static final int SHOOTER_R1_ID = 11;
     public static final int SHOOTER_R2_ID = 12;
 
-    public static final double SHOOTER_SPEED = 1200; // RPM 3 meters 1900 4 meters 2200
+    public static final double SHOOTER_SPEED = -1200; // RPM 3 meters 1900 4 meters 2200
     public static final double ERROR_MARGIN = 100; // RPM
     public static final double STOP = 0;
     public static final double IDLE = 0.1; // % voltage -1 --> 1
 
-    public static final double ALLIANCE_IDLE_RPM = 1000;
+    public static final double ALLIANCE_IDLE_RPM = -1000;
 
     // PID Constants For Shooter
     public static final double p = 0.0002355;
