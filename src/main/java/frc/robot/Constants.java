5a8a9a982d27f2daa5216689f8c387fa07f0ce95
xeldridge18 <xeldridge13@gmail.com>
2026-04-02 13:30:32 -0500

    Update at 'Thu Apr 02 13:30:32 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -183,8 +183,8 @@ public static final double LOOKAHEAD_MAX_SEC   = 1.5;
 
     // Feed-Forward Constants for Shooter
     public static final double s = 0.0;
-    public static final double v = 0.007;
-    public static final double leftv = 0.007;
+    public static final double v = 0.0017;
+    // public static final double leftv = 0.007;
     public static final double a = 0.0;
 
     public final static InterpolatingDoubleTreeMap TOF = new InterpolatingDoubleTreeMap();
