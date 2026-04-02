d441e989f370f5777476a0957e24c8871fb94e10
AwtyBotsOfficial <guptai@awty.org>
2026-04-01 22:55:46 -0500

    david david david

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -508,8 +508,10 @@ public class RobotContainer {
     // // SysId: run shooter dynamic reverse.
     // operatorXbox.y().whileTrue(m_shooter.sysIdDynamicReverse());
 
-    new Trigger(() -> isInAllianceZone() && !m_shooter.isShooterFast()).onTrue(m_shooter.setAllianceIdle());
-    new Trigger(() -> !isInAllianceZone() && !m_shooter.isShooterFast()).onTrue(m_shooter.setNeutralIdle());
+    new Trigger(() -> isInAllianceZone() && !m_shooter.isShooterFast()).onTrue(Commands.runOnce(() -> m_shooter.setDefaultCommand(m_shooter.setAllianceIdle())));
+    new Trigger(() -> !isInAllianceZone() && !m_shooter.isShooterFast()).onTrue(Commands.runOnce(() -> m_shooter.setDefaultCommand(m_shooter.setNeutralIdle())));
+
+    
 
     if (RobotBase.isSimulation()) {
       drivebase.setDefaultCommand(driveFieldOrientedDirectAngleKeyboard);
