b0c8095a1c2a47caabc9fb694ca27a4b2a7fa8e8
1adityatandon1234-netizen <27cuiz@awty.org>
2026-04-23 18:17:11 -0500

    Update at 'Thu Apr 23 18:17:10 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -184,9 +184,9 @@ public final class Constants {
     public static final double NEUTRAL_IDLE_RPM = -0;
 
     // PID Constants For Shooter
-    public static final double p = 0.00023;
+    public static final double p = 0.000236;
     public static final double i = 0.000;
-    public static final double d = 0.00001;
+    public static final double d = 0.0001;
 
     // Feed-Forward Constants for Shooter
     public static final double s = 0.0;
