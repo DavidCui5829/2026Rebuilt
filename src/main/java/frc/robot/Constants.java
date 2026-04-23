21a88cf78fc14470170a34cd0ed762fb8ccd278c
1adityatandon1234-netizen <27cuiz@awty.org>
2026-04-23 17:52:34 -0500

    Update at 'Thu Apr 23 17:52:34 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -184,13 +184,13 @@ public final class Constants {
     public static final double NEUTRAL_IDLE_RPM = -0;
 
     // PID Constants For Shooter
-    public static final double p = 0.000225;
+    public static final double p = 0.00023;
     public static final double i = 0.000;
     public static final double d = 0.00001;
 
     // Feed-Forward Constants for Shooter
     public static final double s = 0.0;
-    public static final double v = 0.0017;
+    public static final double v = 0.001725;
     public static final double a = 0.0;
 
     public final static InterpolatingDoubleTreeMap TOF = new InterpolatingDoubleTreeMap();
