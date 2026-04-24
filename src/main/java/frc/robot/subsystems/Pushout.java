5653487ad2ef39950ba64fb7cf6de08784dc33b1
1adityatandon1234-netizen <27cuiz@awty.org>
2026-04-24 16:44:21 -0500

    Update at 'Fri Apr 24 16:44:20 CDT 2026'

--- src/main/java/frc/robot/subsystems/Pushout.java
+++ src/main/java/frc/robot/subsystems/Pushout.java
@@ -49,7 +49,7 @@ public class Pushout extends SubsystemBase {
 
     private RelativeEncoder pushoutEncoder = PushoutMotor.getEncoder();
 
-    double minVelocity = -450.0;
+    double minVelocity = -350.0;
     // private final RelativeEncoder pushoutRightEncoder =
     // PushoutRightMotor.getEncoder();
 
