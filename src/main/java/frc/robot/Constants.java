615396a33094887813f98d4835f10eb86a5a2789
1adityatandon1234-netizen <27cuiz@awty.org>
2026-04-27 20:16:59 -0500

    Update at 'Mon Apr 27 20:16:58 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -175,7 +175,7 @@ public final class Constants {
 
     public static final double SHOOTER_SPEED = -1735;                  // RPM 3 meters 1900 4 meters 2200
     public static final double SHOOTER_PASSING_SPEED = -4000;  
-    public static final double ERROR_MARGIN = 100; // RPM         
+    public static final double ERROR_MARGIN = 50; // RPM         
     public static final double STOP = 0;
     public static final double IDLE = 0.1; // % voltage -1 --> 1
 
